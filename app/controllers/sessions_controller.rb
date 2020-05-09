0db043b7f91ba9f1c7ed6153f9505ba4d54b7f1b
Kuniaki Igarashi <igaiga@gmail.com>
2020-05-09 10:07:15 +0900

    Implement GitHub login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_or_create_from_auth_hash!(request.env['omniauth.auth']) # ((1))
+    session[:user_id] = user.id # ((2))
+    redirect_to root_path, notice: 'ログインしました' # ((3))
+  end
+end
