0aa21b3ec624473c6353fa2e432979068f9fc3b9
Kuniaki Igarashi <igaiga@gmail.com>
2020-05-13 08:57:39 +0900

    EventsTestの文言を調整

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -1,13 +1,13 @@
 require "application_system_test_case"
 
 class EventsTest < ApplicationSystemTestCase
-  test "/events/:idページを表示" do
+  test "/events/:id ページを表示" do
     event = FactoryBot.create(:event) #((1))
     visit event_url(event) #((2))
     assert_selector "h1", text: event.name #((3))
   end
 
-  test "/events/newを表示" do
+  test "/events/new ページを表示" do
     sign_in_as(FactoryBot.create(:user))
 
     visit new_event_url
