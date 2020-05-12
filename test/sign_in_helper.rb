231eba2a2e20095af7ed960e5a497ee157b872d8
Kuniaki Igarashi <igaiga@gmail.com>
2020-05-13 08:46:09 +0900

    Fix problem in sign-in helper

--- test/sign_in_helper.rb
+++ test/sign_in_helper.rb
@@ -1,13 +1,14 @@
 module SignInHelper
   def sign_in_as(user) #((1))
-    OmniAuth.config.add_mock( #((2))
+    OmniAuth.config.test_mode = true #((2))
+    OmniAuth.config.add_mock( #((3))
       user.provider,
       uid: user.uid,
       info: { nickname: user.name,
               image: user.image_url })
 
-    visit root_url #((3))
-    click_on "GitHubでログイン" #((4))
+    visit root_url #((4))
+    click_on "GitHubでログイン" #((5))
     @current_user = user
   end
 
