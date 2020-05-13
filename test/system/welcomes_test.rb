9e3668c09b0db4f51c9bc2b455307434c0cea54a
Kuniaki Igarashi <igaiga@gmail.com>
2020-05-13 09:47:51 +0900

    Add Welcomes#index future and past events test

--- test/system/welcomes_test.rb
+++ test/system/welcomes_test.rb
@@ -1,9 +1,18 @@
 require "application_system_test_case"
 
 class WelcomesTest < ApplicationSystemTestCase
-  test "/ページを表示" do
+  test "/ ページを表示" do
     visit root_url
 
     assert_selector "h1", text: "イベント一覧"
+  end
+
+  test "/ ページを表示して、未来のイベントが表示され、過去のイベントが表示されないこと" do
+    future_event = FactoryBot.create(:event, start_at: Time.zone.now + 1.day)
+    past_event = FactoryBot.create(:event, start_at: Time.zone.now - 1.day)
+
+    visit root_url
+    assert_selector "h5", text: future_event.name
+    assert_no_selector "h5", text: past_event.name
   end
 end
