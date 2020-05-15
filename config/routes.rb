ef1c5568277902d04761619fe438cef317c26e56
Kuniaki Igarashi <igaiga@gmail.com>
2020-05-15 11:14:01 +0900

    Add /status API

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy", as: :logout
+  get 'status' => 'status#index', defaults: { format: 'json' }
 
   resource :retirements, only: %i[new create]
   resources :events do
--- test/controllers/status_controller_test.rb
+++ test/controllers/status_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class StatusControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "GET /status" do
+    get "/status" #((1))
+    assert_response(:success) #((2))
+    assert_equal({ status: "ok" }.to_json, @response.body) #((3))
+    assert_equal("application/json", @response.media_type) #((4))
+  end
 end
