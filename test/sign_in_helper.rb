0aca64e505a611158aea06a4174c4e080150a794
Kuniaki Igarashi <igaiga@gmail.com>
2020-05-15 12:51:35 +0900

    Update SignInHelper for IntegrationTest

--- test/sign_in_helper.rb
+++ test/sign_in_helper.rb
@@ -1,18 +1,29 @@
 module SignInHelper
-  def sign_in_as(user) #((1))
-    OmniAuth.config.test_mode = true #((2))
-    OmniAuth.config.add_mock( #((3))
+  def sign_in_as(user)
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.add_mock(
       user.provider,
       uid: user.uid,
       info: { nickname: user.name,
               image: user.image_url })
 
-    visit root_url #((4))
-    click_on "GitHubでログイン" #((5))
+    case #((1))
+    when respond_to?(:visit)
+      visit root_url
+      click_on "GitHubでログイン"
+    when respond_to?(:get)
+      get "/auth/github/callback"
+    else
+      raise NotImplementedError.new
+    end
     @current_user = user
   end
 
   def current_user
     @current_user
   end
+end
+
+class ActionDispatch::IntegrationTest
+  include SignInHelper #((2))
 end
