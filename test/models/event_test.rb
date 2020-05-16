e84f6451e88d1d3fc6f680cbdd7f8185cb4996d4
Kuniaki Igarashi <igaiga@gmail.com>
2020-05-16 13:09:07 +0900

    Add Stub test code in Event model test

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -13,4 +13,22 @@ class EventTest < ActiveSupport::TestCase
     event = FactoryBot.create(:event)
     assert_equal(false, event.created_by?(nil))
   end
+
+  # stub
+  # test '#created_by? owner_id と 引数の#id が同じとき(stub)' do
+  #   event = FactoryBot.create(:event)
+  #   user = User.new #((1))
+  #   user.stub(:id, event.owner_id) do #((2))
+  #     assert_equal(true, event.created_by?(user)) #((3))
+  #   end
+  # end
+  # test '#created_by? owner_id と 引数の#id が異なるとき(stub)' do
+  #   event = FactoryBot.create(:event)
+  #   user = User.new
+  #   another_user = FactoryBot.create(:user)
+  #   user.stub(:id, event.owner_id) do
+  #     assert_equal(false, event.created_by?(another_user))
+  #   end
+  # end
+
 end
