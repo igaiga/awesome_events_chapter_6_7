86e1a9cffe02d73ddeab52e030776cb1ccb2d123
Kuniaki Igarashi <igaiga@gmail.com>
2020-05-16 10:05:25 +0900

    Add detroy test in EventsControllerTest

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
-
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "自分がつくったイベントは削除できる" do
+    event_owner = FactoryBot.create(:user)
+    event = FactoryBot.create(:event, owner: event_owner)
+    sign_in_as event_owner #((1))
+    assert_difference("Event.count", -1) do #((2))
+      delete event_url(event) #((3))
+    end
+  end
+
+  test "他の人がつくったイベントは削除できない" do
+    event_owner = FactoryBot.create(:user) #((1))
+    event = FactoryBot.create(:event, owner: event_owner)
+    sign_in_user = FactoryBot.create(:user)
+    sign_in_as sign_in_user
+    assert_difference("Event.count", 0) do #((2))
+      assert_raises(ActiveRecord::RecordNotFound) do #((3))
+        delete event_url(event) #((4))
+      end
+    end
+  end
 end
